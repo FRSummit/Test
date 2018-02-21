0605c3dc0a4474fdc8a68c4d9fe02b8523d12397
F R Summit <www.frsummit1@gmail.com>
2018-02-21 22:43:36 +0600

    Initial commit

---
+++ app/src/main/java/com/frs/frsummit/test/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.frs.frsummit.test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Burger", "Pizza", "Chicken Toast", "Steak", "Mutton", "Fish kary")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener(){
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodName.text = foodList[randomFood]
+        }
+    }
+}
